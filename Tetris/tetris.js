10fca5616f23d2e7b34c05b129a6cee0c610efa2
michaelhoang2019 <michaelhoang2019@gmail.com>
2022-05-27 01:48:25 -0700

    first commit

---
+++ Tetris/tetris.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const grid = document.querySelector(".grid");
+  let squares = Array.from(document.querySelectorAll(".grid div"));
+  const ScoreDisplay = document.querySelector("#score");
+  const StartBtn = document.querySelector("start-button");
+  const width = 10;
+
+  //The Tetrominoes
+  const lTetromino = [[1, width + 1, width * 2 + 1, 2], [,]];
+});
